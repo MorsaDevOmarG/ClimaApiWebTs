f70f6db5682ca3cf98d1ba544312e25d9170265d
Omar <omar.garcia@morsa.com.mx>
2025-12-18 00:13:12 -0600

    Escribiendo en el STATE

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,38 +1,57 @@
+import { useState, type ChangeEvent } from "react";
 import { countries } from "../../data/countries";
 import styles from "./Form.module.css";
+import type { SearchType } from "../../types";
 
 export default function Form() {
+  const [search, setSearch] = useState<SearchType>({
+    city: "",
+    country: "",
+  });
+
+  const handleChange = (
+    e: ChangeEvent<HTMLInputElement> | ChangeEvent<HTMLSelectElement>
+  ) => {
+    setSearch({
+      ...search,
+      [e.target.name]: e.target.value,
+    });
+  };
+
   return (
     <form className={styles.form}>
       <div className={styles.field}>
         <label htmlFor="city">Ciudad</label>
 
-        <input type="text" name="city" id="city" placeholder="Ingresa la Ciudad" />
+        <input
+          type="text"
+          name="city"
+          id="city"
+          placeholder="Ingresa la Ciudad"
+          value={search.city}
+          onChange={handleChange}
+        />
       </div>
 
-      <div>
-        <label htmlFor="city">País</label>
+      <div className={styles.field}>
+        <label htmlFor="country">País</label>
 
-        <select name="" id="">
+        <select
+          id="country"
+          value={search.country}
+          name="country"
+          onChange={handleChange}
+        >
           <option value="">-- Seleccione un país --</option>
-          {
-            countries.map(
-              country => (
-                <option
-                  key={country.code}
-                  value={country.code}
-                >
-                  {
-                    country.name
-                  }
-                </option>
-              )
-            )
-          }
+          {countries.map((country) => (
+            <option key={country.code} value={country.code}>
+              {country.name}
+            </option>
+          ))}
         </select>
       </div>
 
-      <input type="submit" value='Consultar Clima' className={styles.submit} />
+      <input type="submit" value="Consultar Clima" className={styles.submit} />
     </form>
   );
-};
+}
